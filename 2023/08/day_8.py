9dcff3cb701d032cf72e27270ad0d672cc8c3479
Travers <traversannan@yahoo.ca>
2024-08-07 11:55:26 -0400

    finished day 8

--- 2023/08/day_8.py
+++ 2023/08/day_8.py
@@ -1,31 +1,32 @@
+from math import lcm
+from functools import reduce
+
+
 def part_1(input):
     camel_path = input[0]
     node_list = input[1].split("\n")
-    print(node_list)
+
     path_map = {"L": 0, "R": 1}
 
     node_map = {}
 
     for node in node_list:
-        print(f"node: {node}")
         node_split = node.split(" = ")
         key = node_split[0]
         value = node_split[-1].replace("(", "").replace(")", "").split(", ")
 
         node_map[key] = (value[0], value[1])
 
-    print(node_map)
-
     node = "AAA"
     steps = 0
-    print(f"{steps}: {node}")
 
     while node != "ZZZ":
         for char in camel_path:
             steps += 1
             next_node = node_map[node][path_map[char]]
-            print(f"{steps}: {next_node}")
             node = next_node
+
+    print(f"{steps}: {node}")
 
 
 def part_2(input):
@@ -36,38 +37,38 @@ def part_2(input):
 
     node_map = {}
 
-    starting_nodes = []
+    nodes = []
 
     for node in node_list:
-        print(f"node: {node}")
-        node_split = node.split(" = ")
-        key = node_split[0]
-        value = node_split[-1].replace("(", "").replace(")", "").split(", ")
+        key = node[0:3]
+        left_value = node[7:10]
+        right_value = node[12:15]
 
-        if key[-1] == "A":
-            starting_nodes.append(key)
+        if key[2] == "A":
+            nodes.append(key)
 
-        node_map[key] = (value[0], value[1])
+        node_map[key] = (left_value, right_value)
 
-    print(starting_nodes)
-
-    nodes = starting_nodes.copy()
     steps = 0
 
-    # brute force not effective here, need to use LCM bc loops are cyclical
-    while True:
+    # brute force not effective here, need to use LCM bc these loops happen to be cyclical
+    y = []
+    z = 0
+    e = len(nodes)
+    print(nodes)
+    while z < e:
         for char in camel_path:
-            # print(nodes)
             steps += 1
-            # print(steps)
-            for i, node in enumerate(nodes):
-                next_node = node_map[node][path_map[char]]
-                # print(f"{steps}: {next_node}")
-                nodes[i] = next_node
-            node_ends = [node[-1] for node in nodes]
-            print(f"{steps}: {node_ends}")
-            if all(node_ends) == "Z":
-                break
+            for i in range(len(nodes)):
+                nodes[i] = node_map[nodes[i]][path_map[char]]
+            for x in nodes:
+                if x[2] == "Z":
+                    z += 1
+                    print(nodes)
+                    nodes.remove(x)
+                    y.append(steps)
+    print(y)
+    print(reduce(lcm, y))
 
 
 if __name__ == "__main__":
